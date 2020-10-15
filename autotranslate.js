73f06775b90c03ad8846acf007bb824e61b102b0
Paolo Rampino <liadserv@gmail.com>
2020-10-15 23:54:38 +0200

    Make it as tampermonkey script

--- autotranslate.js
+++ autotranslate.js
@@ -1,4 +1,11 @@
-//Script written by Paolo Rampino
+// ==UserScript==
+// @name         Replica Translator
+// @version      0.1
+// @description  try to take over the world!
+// @author       Paolo Rampino
+// @match        https://my.replika.ai/
+// @grant    none
+// ==/UserScript==
 
 //License: MIT
 
@@ -11,81 +18,92 @@ var myKey = "YOUR-GOOGLE-API-KEY-HERE";
 //set your lang
 var lang = 'it';
 
-// this caches your Input
-var allIsaid = '';
-
-function getTranslation(tid, term, target, useVal = false) {
-    var url = "https://translation.googleapis.com/language/translate/v2";
-    url += "?q=" + atob(term);
-    url += "&q=" + encodeURIComponent(tid);
-    url += "&target=" + target;
-    url += "&key=" + myKey;
-    jQuery.get(url, function(data, status) {
-        if (useVal) {
-            jQuery('#' + tid).val(data.data.translations[0].translatedText);
-        } else {
-            jQuery('#' + tid).html('<hr>' + data.data.translations[0].translatedText);
-        }
-    });
-};
-
-function checkTranslation() {
-    jQuery('[id ^=message][id $=-text]').each((i, e) => {
-        if (!jQuery(e).hasClass('trans')) {
-            let tid = "Translated-" + jQuery(e).attr('id');
-            let term = btoa(encodeURIComponent(jQuery(e).text()));
-            try {
-                jQuery(e).html(
-                    jQuery(e).html() + `<br><span id="` + tid + `"><button onclick="getTranslation('` + tid + `', '` + term + `', '` + lang + `')" >T</button></span>`);
-                jQuery(e).addClass('trans');
-            } catch {}
-        }
-    });
-}
-
-function translationLoop() {
-    setTimeout(
-        () => {
-            checkTranslation();
-            translationLoop();
-            addButtons();
-        }, 100
-    )
-}
-
-
-
-function addButtons() {
-    if (!jQuery('#toTranslate').length) {
-        jQuery('[data-testid=chat-controls-emoji-keyboard-button]').parent().prepend('<button id="toTranslate" style="display:inline" onclick="translateTo()">T</button><button id="fromTranslate" style="display:none" onclick="translateFrom()" >U</button>');
-        jQuery('[id=send-message-textarea]').on('keyup', (i, e) => {
-            unTrans();
-        });
-    }
-}
-
-function translateTo() {
-    allIsaid = jQuery('[id=send-message-textarea]').val();
-    jQuery('#toTranslate').attr('style', 'display: none');
-    jQuery('#fromTranslate').attr('style', 'display: inline');
-    getTranslation("send-message-textarea", btoa(encodeURIComponent(allIsaid)), 'en', true)
-}
-
-function unTrans() {
-    jQuery('#fromTranslate').attr('style', 'display: none');
-    jQuery('#toTranslate').attr('style', 'display: inline');
-}
-
-function translateFrom() {
-    jQuery('#fromTranslate').attr('style', 'display: none');
-    jQuery('#toTranslate').attr('style', 'display: inline');
-    jQuery('[id=send-message-textarea]').val(allIsaid);
-}
-
 javascript: (function(e, s) {
     e.src = s;
     e.onload = function() {
         jQuery.noConflict();
+        jQuery('body').append(`<script>
+        var lang = '` + lang + `';
+        function translationLoop() {
+            setTimeout(
+                () => {
+                    checkTranslation();
+                    translationLoop();
+                    addButtons();
+                }, 100
+            )
+        }
+        
+        function checkTranslation() {
+            jQuery('[id ^=message][id $=-text]').each((i, e) => {
+                if (!jQuery(e).hasClass('trans')) {
+                    let tid = "Translated-" + jQuery(e).attr('id');
+                    let term = btoa(encodeURIComponent(jQuery(e).text()));
+                    try {
+                        jQuery(e).html(
+                            jQuery(e).html() + '<br><span id="' + tid + '"><button onclick="'+\`getTranslation('\` + tid + \`', '\` + term + \`', '\` + lang + \`')" \` + '>T</button></span>');
+                        jQuery(e).addClass('trans');
+                    } catch {}
+                }
+            });
+        }
+        
+        function translationLoop() {
+            setTimeout(
+                () => {
+                    checkTranslation();
+                    translationLoop();
+                    addButtons();
+                }, 100
+            )
+        }
+        
+        function getTranslation(tid, term, target, useVal = false) {
+            var url = "https://translation.googleapis.com/language/translate/v2";
+            url += "?q=" + atob(term);
+            url += "&q=" + encodeURIComponent(tid);
+            url += "&target=" + target;
+            url += "&key=` + myKey + `";
+            jQuery.get(url, function(data, status) {
+                if (useVal) {
+                    jQuery('#' + tid).val(data.data.translations[0].translatedText);
+                } else {
+                    jQuery('#' + tid).html('<hr>' + data.data.translations[0].translatedText);
+                }
+            });
+        };
+        // this caches your Input
+        var allIsaid = '';
+        
+        function addButtons() {
+            if (!jQuery('#toTranslate').length) {
+                jQuery('[data-testid=chat-controls-emoji-keyboard-button]').parent().prepend('<button id="toTranslate" style="display:inline" onclick="translateTo()">T</button><button id="fromTranslate" style="display:none" onclick="translateFrom()" >U</button>');
+                jQuery('[id=send-message-textarea]').on('keyup', () => {
+                    unTrans();
+                });
+            }
+        }
+        
+        function translateTo() {
+            allIsaid = jQuery('[id=send-message-textarea]').val();
+            jQuery('#toTranslate').attr('style', 'display: none');
+            jQuery('#fromTranslate').attr('style', 'display: inline');
+            getTranslation("send-message-textarea", btoa(encodeURIComponent(allIsaid)), 'en', true)
+        }
+        
+        function unTrans() {
+            jQuery('#fromTranslate').attr('style', 'display: none');
+            jQuery('#toTranslate').attr('style', 'display: inline');
+        }
+        
+        function translateFrom() {
+            jQuery('#fromTranslate').attr('style', 'display: none');
+            jQuery('#toTranslate').attr('style', 'display: inline');
+            jQuery('[id=send-message-textarea]').val(allIsaid);
+        }
+        </script>
+        `);
+
         translationLoop();
         addButtons();
     };
